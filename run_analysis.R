59de34a3678005f746cfe080a3a7c9191d3e6f15
Matthew Que <matthew.neale.que@gmail.com>
2018-12-24 19:54:22 +0800

    Updated README, CodeBook, and run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -41,15 +41,6 @@ activityLabels <- read_lines(".\\activity_labels.txt")
 # read training data sets
 
 x <- convertNumericList(read_lines(".\\train\\X_train.txt"))
-bodyaccx <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_acc_x_train.txt"))
-bodyaccy <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_acc_y_train.txt"))
-bodyaccz <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_acc_z_train.txt"))
-bodygyrox <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_gyro_x_train.txt"))
-bodygyroy <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_gyro_y_train.txt"))
-bodygyroz <- convertNumericList(read_lines(".\\train\\Inertial Signals\\body_gyro_z_train.txt"))
-totalaccx <- convertNumericList(read_lines(".\\train\\Inertial Signals\\total_acc_x_train.txt"))
-totalaccy <- convertNumericList(read_lines(".\\train\\Inertial Signals\\total_acc_y_train.txt"))
-totalaccz <- convertNumericList(read_lines(".\\train\\Inertial Signals\\total_acc_z_train.txt"))
 
 # create training data set
 
@@ -68,33 +59,9 @@ for(i in featuresFilter){
     datasetTrain <- datasetTrain %>% mutate(!!colName := retrieveData(x, i)) 
 }
 
-# tidy up signal values to be separated to different columns
-
-for(i in seq_along(1:128)){
-    datasetTrain <- datasetTrain %>% mutate(!!paste0("bodyaccelsignalx-", i) := retrieveData(bodyaccx, i),
-                                            !!paste0("bodyaccelsignaly-", i) := retrieveData(bodyaccy, i),
-                                            !!paste0("bodyaccelsignalz-", i) := retrieveData(bodyaccz, i),
-                                            !!paste0("bodygyrosignalx-", i) := retrieveData(bodygyrox, i),
-                                            !!paste0("bodygyrosignaly-", i) := retrieveData(bodygyroy, i),
-                                            !!paste0("bodygyrosignalz-", i) := retrieveData(bodygyroz, i),
-                                            !!paste0("bodygyrosignalx-", i) := retrieveData(bodygyrox, i),
-                                            !!paste0("phoneaccelsignalx-", i) := retrieveData(totalaccx, i),
-                                            !!paste0("phoneaccelsignaly-", i) := retrieveData(totalaccy, i),
-                                            !!paste0("phoneaccelsignalz-", i) := retrieveData(totalaccz, i))
-}
-
 # read test data sets
 
 x <- convertNumericList(read_lines(".\\test\\X_test.txt"))
-bodyaccx <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_acc_x_test.txt"))
-bodyaccy <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_acc_y_test.txt"))
-bodyaccz <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_acc_z_test.txt"))
-bodygyrox <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_gyro_x_test.txt"))
-bodygyroy <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_gyro_y_test.txt"))
-bodygyroz <- convertNumericList(read_lines(".\\test\\Inertial Signals\\body_gyro_z_test.txt"))
-totalaccx <- convertNumericList(read_lines(".\\test\\Inertial Signals\\total_acc_x_test.txt"))
-totalaccy <- convertNumericList(read_lines(".\\test\\Inertial Signals\\total_acc_y_test.txt"))
-totalaccz <- convertNumericList(read_lines(".\\test\\Inertial Signals\\total_acc_z_test.txt"))
 
 datasetTest <- data.frame(subject = as.numeric(read_lines(".\\test\\subject_test.txt")),
                           activity = read_lines(".\\test\\y_test.txt"),
@@ -109,21 +76,6 @@ datasetTest <- datasetTest %>% mutate(activity = sapply(activity, function(x) fi
 for(i in featuresFilter){
     colName <- sub("^[0-9]+ ", "", features[i])
     datasetTest <- datasetTest %>% mutate(!!colName := retrieveData(x, i)) 
-}
-
-# tidy up signal values to be separated to different columns
-
-for(i in seq_along(1:128)){
-    datasetTest <- datasetTest %>% mutate(!!paste0("bodyaccelsignalx-", i) := retrieveData(bodyaccx, i),
-                                          !!paste0("bodyaccelsignaly-", i) := retrieveData(bodyaccy, i),
-                                          !!paste0("bodyaccelsignalz-", i) := retrieveData(bodyaccz, i),
-                                          !!paste0("bodygyrosignalx-", i) := retrieveData(bodygyrox, i),
-                                          !!paste0("bodygyrosignaly-", i) := retrieveData(bodygyroy, i),
-                                          !!paste0("bodygyrosignalz-", i) := retrieveData(bodygyroz, i),
-                                          !!paste0("bodygyrosignalx-", i) := retrieveData(bodygyrox, i),
-                                          !!paste0("phoneaccelsignalx-", i) := retrieveData(totalaccx, i),
-                                          !!paste0("phoneaccelsignaly-", i) := retrieveData(totalaccy, i),
-                                          !!paste0("phoneaccelsignalz-", i) := retrieveData(totalaccz, i))
 }
 
 # merge data sets
